9a32c5cbc75ba5e89c89cfdeda53098b23ddbdf0
Antonio Dias <antonio.dias@bit.com.br>
2022-06-10 18:01:12 -0300

    first commit

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public string cpf;
+        public double salario;
+
+        public double getBonificacao()
+        {
+            return salario * 0.1;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using bytebank_ADM.Funcionarios;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao ByteBank Administração");
+
+        Funcionario joao = new Funcionario();
+        joao.nome = "Joao Pinto da Silva";
+        joao.cpf = "12123123";
+        joao.salario = 5000;
+
+        Console.WriteLine("Bonificação: " + joao.getBonificacao());
+    }
+}
